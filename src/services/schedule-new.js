9c4d10b6ed3c87e15e45b1225d67226272c623e1
Paul Anderson <pauldevop@gmail.com>
2024-06-20 18:38:43 -0300

    Adding schedule fetch by day

--- src/services/schedule-new.js
+++ src/services/schedule-new.js
@@ -2,6 +2,7 @@ import { apiConfig } from "./api-config";
 
 export async function scheduleNew({ id, name, when }) {
     try {
+        // Faz a requisição para enviar os dados do agendamento
         await fetch(`${apiConfig.baseURL}/schedules`, {
             method: 'POST',
             headers: {
@@ -9,7 +10,7 @@ export async function scheduleNew({ id, name, when }) {
             },
             body: JSON.stringify({ id, name, when })
         })
-
+        // Exibe mensagem de agendamento realizado
         alert("Agendamento realizado com sucesso!")
     } catch (error) {
         console.log(error)
