6afb5c6c96e8853cd73d0205a57a72155d5b8694
Paul Anderson <pauldevop@gmail.com>
2024-06-20 15:08:53 -0300

    Adding next available hours

---
+++ src/modules/form/date-change.js
@@ -0,0 +1,7 @@
+import { scheduleDay } from "../schedules/load"
+
+// Seleciona o input de data
+const selectedDate = document.getElementById("date")
+
+// Recarrega a lista de horários quando o input de data mudar.
+selectedDate.onchange = () => scheduleDay()
