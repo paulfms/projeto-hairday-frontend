f7b225d2587e6cf62bb7ebed66c8ccb7b32236f8
Paul Anderson <pauldevop@gmail.com>
2024-06-20 20:09:23 -0300

    Selecting schedules to remove

---
+++ src/modules/schedules/cancel.js
@@ -0,0 +1,24 @@
+const periods = document.querySelectorAll(".period")
+
+
+// Gera evento click para cada lista (manhã, tarde e noite)
+periods.forEach((period)=> {
+    // Captura o evento de clique na lista.
+    period.addEventListener("click", (event) =>{
+        if(event.target.classList.contains("cancel-icon")){
+            // Obtém a li pai do elemento clicado.
+            const item = event.target.closest("li")
+            const { id } = item.dataset
+
+            if (id){
+                const isConfirm = confirm("Tem certeza que deseja cancelar o agendamento?"
+
+                )
+
+                if(isConfirm){
+                    console.log("REMOVER!")
+                }
+            }
+        }
+    })
+})
