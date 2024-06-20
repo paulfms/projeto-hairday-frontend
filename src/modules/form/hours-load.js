51e8190f24470108364d279b1e258dd73b4730b8
Paul Anderson <pauldevop@gmail.com>
2024-06-20 13:52:49 -0300

    Sctructuring opening hours and files

---
+++ src/modules/form/hours-load.js
@@ -0,0 +1,19 @@
+import dayjs from "dayjs"
+import { openingHours } from "../../utils/opening-hours.js"
+
+export function hoursLoad({ date }){
+    const opening = openingHours.map((hour) =>{
+        // Recupera somente a hora.
+        const [scheduleHour] = hour.split(":")
+
+        //Adiciona a hora na date e verificar se está no passado.
+        const isHourPast = dayjs(date).add(scheduleHour, "hour").isAfter(dayjs())
+        
+        return {
+            hour,
+            available: isHourPast,
+        }
+    })
+
+    console.log(opening)
+}
