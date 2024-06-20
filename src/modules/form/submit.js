bd56c7ab43c35a2074a5352c90031f76a64c536c
Paul Anderson <pauldevop@gmail.com>
2024-06-20 18:28:49 -0300

    Registering a new schedule on API

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,5 +1,7 @@
 import dayjs from "dayjs"
 
+import { scheduleNew } from "../../services/schedule-new"
+
 const form = document.querySelector("form")
 const clientName = document.getElementById("client")
 const selectedDate = document.getElementById("date")
@@ -13,7 +15,7 @@ selectedDate.value = inputToday
 // Define a data mínima como sendo a data atual.
 selectedDate.min = inputToday
 
-form.onsubmit = (event) => {
+form.onsubmit = async (event) => {
     event.preventDefault()
 
     try {
@@ -40,12 +42,12 @@ form.onsubmit = (event) => {
         // Gera um ID
         const id = new Date().getTime()
 
-        console.log({
+        await scheduleNew({
             id,
             name,
             when
         })
-        
+
     } catch (error) {
         alert("Não foi possível realizar o agendamento")
         console.log(error)
