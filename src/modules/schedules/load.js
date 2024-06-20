cb664c1e123a16e7772a786f07809dce675fff5f
Paul Anderson <pauldevop@gmail.com>
2024-06-20 18:50:09 -0300

    Adding schedules fetch on API

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,12 +1,16 @@
+import { scheduleFetchByDay } from "../../services/schedule-fetch-by-day";
 import { hoursLoad } from "../form/hours-load";
 
 // Seleciona o input de data.
 const selectedDate = document.getElementById("date")
 
-export function scheduleDay() {
+export async function scheduleDay() {
     // Obtém a data do input
     const date = selectedDate.value
 
+    // Busca na API os agendamentos
+    const dailySchedules = await scheduleFetchByDay({ date })
+    console.log(dailySchedules)
 
     // Renderiza as horas disponíveis.
     hoursLoad({ date })
