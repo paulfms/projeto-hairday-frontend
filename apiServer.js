c7ca6195ddd3be32f926d339a0d4d0dbe7ed457b
Paul Anderson <pauldevop@gmail.com>
2024-07-09 13:02:40 -0300

    Creating file config to deploy json-server

---
+++ apiServer.js
@@ -0,0 +1,15 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("server.json");
+
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+
+server.use(jsonServer.rewriter());
+
+server.use(router);
+
+server.listen(3000, () => {});
+
+module.exports = server;
